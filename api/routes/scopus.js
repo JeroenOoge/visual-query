36b66d51dfed1e7edeafbcdb04ea429ec2579681
Jeroen <33607235+JeroenOoge@users.noreply.github.com>
2021-12-09 11:27:06 +0100

    Try to fix the https in the backend

--- api/routes/scopus.js
+++ api/routes/scopus.js
@@ -2,14 +2,22 @@ var express = require("express");
 var router = express.Router();
 const https = require('https');
 
-router.get("/:str/:key", async function (req, res, next) {
+router.get("/:str/:key", function (req, result, next) {
     const httpAccept = encodeURIComponent("httpAccept=application/json"),
         queryString = "&query=" + req.params["str"],
         key = "&apiKey=" + req.params["key"],
         countString = "&count=" + 1;
-    let response = await https.get("https://api-elsevier-com.kuleuven.e-bronnen.be/content/search/scopus?" + httpAccept + queryString + key + countString);
-    // let response = [queryString, key, countString];
-    res.send(response);
+    https.get("https://api-elsevier-com.kuleuven.e-bronnen.be/content/search/scopus?" + httpAccept + queryString + key + countString, (res) => {
+        console.log('statusCode:', res.statusCode);
+        console.log('headers:', res.headers);
+
+        res.on('data', (d) => {
+            result.send(d);
+        });
+
+    }).on('error', (e) => {
+        console.error(e);
+    });
 });
 
 module.exports = router;
